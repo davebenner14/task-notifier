2c16c993556368d039902b8ec590394805080ad6
David Benner <davidbenner@davids-mbp.lan>
2023-06-22 16:36:31 -0400

    created Task view and Task component

---
+++ src/views/Tasks.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <TaskForm @add-task="addTask" />
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TaskForm from "../components/TaskForm.vue";
+
+export default {
+  components: {
+    TaskForm,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks.push(task);
+    },
+  },
+};
+</script>
